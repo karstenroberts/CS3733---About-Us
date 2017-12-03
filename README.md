387d8a096e44a555476bc5b62ba43a349363f881
karstenroberts <karstenroberts@gmail.com>
2017-12-02 19:34:54 -0500

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+Assignment 2 - Basics HTML and CSS
+===
+
+Karsten Roberts
+http://a2-karstenroberts.herokuapp.com
+
+This project shows ...
+
+## Technical Achievements
+I figured out how to auto-hide the lists in drop down menus that only expand
+  after hovering over the dropdown title.
+
+### Design Achievements
+Thematic music plays when the page is displayed, with the music controller
+  hidden and music playing on repeat. 
+Background color was set to match the 
+  outer edges of image, to make the transition more or less seamless (color of
+  image not perfectly uniform)
+On hover over image, alt-text is displayed
+
+
---
+++ server.js
@@ -0,0 +1,46 @@
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
+
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
+
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'public/index.html')
+      break
+    case '/index.html':
+      sendFile(res, 'public/index.html')
+      break
+    case '/Doors.png':
+      sendFile(res, 'public/Doors.png')
+      break
+    case '/DFSong.mp3':
+      sendFile(res, 'public/DFSong.mp3')
+      break
+    case '/css/style.css':
+      sendFile(res, 'public/css/style.css', 'text/css')
+      break
+    case '/js/scripts.js':
+      sendFile(res, 'public/js/scripts.js', 'text/javascript')
+      break
+    default:
+      res.end('404 not found')
+  }
+})
+
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+// subroutines
+
+function sendFile(res, filename, contentType) {
+  contentType = contentType || 'text/html';
+
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': contentType})
+    res.end(content, 'utf-8')
+  })
+
+}
